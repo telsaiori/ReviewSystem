6fbde5b6b6a6f5d8fa47b0eb4c995f153d3b45a4
telsa <telsaiori.git@gmail.com>
2017-01-28 18:53:17 +0800

    add ajax for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,10 +4,13 @@ class CommentsController < ApplicationController
     @event = Event.find(params[:event_id])
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build(comment_params)
-    if @comment.save
-      redirect_to event_post_path(@event, @post), notice: 'Saved'
-    else
-      redirect_to :back
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to event_post_path(@event, @post), notice: 'Saved' }
+        format.js
+      else
+        format.html { redirect_back(fallback_location: root_path) }
+      end
     end
   end
 
